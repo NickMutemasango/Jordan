f58f509ca106d429506d130139743a68587b38d2
NickMutemasango <geniusnickie@gmail.com>
2024-10-24 14:56:24 +0200

    changes

--- src/app/jordan/page.tsx
+++ src/app/jordan/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import React from "react";
 import Image from "next/image";
 import Home from "../../Images/Home.svg";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../../variants";
-import Shadow from '../../Images/shadow.svg'
+import Shadow from '../../Images/shadow.svg';
 
 const page = () => {
   return (
@@ -16,18 +16,22 @@ const page = () => {
       exit="hidden"
       className="flex flex-col md:flex-row"
     >
-      <div className="md:w-1/2 md:relative flex flex-col ">
+      <div className="md:w-1/2 md:relative flex flex-col">
         <Image src={Home} alt="" />
-        <Image src={Shadow} alt="" className="hidden md:flex md:w-[80%] md:absolute md:bottom-0 md:right-5"/>
+        <Image
+          src={Shadow}
+          alt=""
+          className="hidden md:flex md:w-[80%] md:absolute md:bottom-0 md:right-5"
+        />
       </div>
       <div className="px-4 md:w-1/2 md:pt-10 xl:pt-16 space-y-3">
         <h1 className="font-bold text-5xl">AIR JORDAN</h1>
         <h4 className="text-3xl font-medium">1 MID - RED</h4>
         <p className="font-medium text-sm xl:text-xl">
           Inspired by the original that debuted in 1985, the Air Jordan 1 Low
-          offers a clean, classic look that's familiar yet always fresh. With an
-          iconic design that pairs perfectly with any 'fit, these kicks ensure
-          you'll always be on point.
+          offers a clean, classic look that&apos;s familiar yet always fresh. With an
+          iconic design that pairs perfectly with any &apos;fit, these kicks ensure
+          you&apos;ll always be on point.
         </p>
         <div className="flex space-x-6 xl:space-x-16 xl:pt-3">
           <h2 className="font-extrabold text-2xl">SIZE</h2>
